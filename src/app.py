303ce1c6cfa6dd95a4e92ec5c23dc4aa863be97e
Preston Moore <moorepjay@gmail.com>
2019-01-23 01:04:27 -0500

    Basic website using flask.

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/widget")
+def do_stuff():
+    return render_template("widget.html")
+
+
+if __name__ == '__main__':
+    app.run()
